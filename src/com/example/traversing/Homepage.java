11b7bc26a117d0256debe6070a20d28995ab5506
Wheat <Wheat@WheatdeMacBook-Pro.local>
2014-12-08 00:39:33 +0800

    12.8

--- src/com/example/traversing/Homepage.java
+++ src/com/example/traversing/Homepage.java
@@ -4,21 +4,27 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Homepage extends Activity {
 
-	private SimpleAdapter adapter;
+	// private SimpleAdapter adapter;
+	private List<Hashtable<String, Object>> data;
 	private String[] news = {
 			"1. USNews global university rankings.",
 			"2. USNews national university rankings.",
@@ -39,7 +45,7 @@ public class Homepage extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.homepage);
-		
+
 		Button button1 = (Button) findViewById(R.id.button_register);
 		button1.setOnClickListener(new OnClickListener() {
 
@@ -51,7 +57,7 @@ public class Homepage extends Activity {
 			}
 
 		});
-		
+
 		Button button2 = (Button) findViewById(R.id.button_login);
 		button2.setOnClickListener(new OnClickListener() {
 
@@ -64,47 +70,39 @@ public class Homepage extends Activity {
 
 		});
 
-		// set content for listView  getdata是下面的结构
-		adapter = new SimpleAdapter(Homepage.this, getData(),
-				R.layout.listitem, new String[] { "news" },
-				new int[] { R.id.news });//一行中有一个textview news
-		ListView result_list = (ListView) findViewById(R.id.list_news);
-		result_list.setAdapter(adapter);
-		
-        /*image button
-		ImageButton imagebutton = (ImageButton)findViewById(R.id.like);
-		imagebutton.setOnTouchListener(new View.OnTouchListener() {
-			
-			@SuppressLint("ClickableViewAccessibility") @Override
-			public boolean onTouch(View v, MotionEvent event) {
-				// TODO Auto-generated method stub
-				if (event.getAction() == MotionEvent.ACTION_DOWN){
-					//重新设置按下时的背景图片  
-		               ((ImageButton)v).setImageDrawable(getResources().getDrawable(R.drawable.icon));
-				}else if(event.getAction() == MotionEvent.ACTION_UP){       
-	                //再修改为抬起时的正常图片  
-	                ((ImageButton)v).setImageDrawable(getResources().getDrawable(R.drawable.beijingtu));             
-				}
-				return false;
-			}
-				
-			
-		});*/
-		
-		//set OnClick for VisitorAssess
-		Button assess = (Button)findViewById(R.id.button_assess);
-		assess.setOnClickListener(new OnClickListener(){
+		// set OnClick for VisitorAssess
+		Button assess = (Button) findViewById(R.id.button_assess);
+		assess.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				Intent visitor_assess = new Intent(Homepage.this, Visitorassess.class);
+				Intent visitor_assess = new Intent(Homepage.this,
+						Visitorassess.class);
 				Homepage.this.startActivity(visitor_assess);
 			}
-			
+
 		});
+
+		ListView result_list = (ListView) findViewById(R.id.list_news);
+		data = getData();
+		MyAdapter adapter = new MyAdapter(this);
+		result_list.setAdapter(adapter);
+		result_list.setOnItemClickListener(listener);
 	}
-	
-    //产生《》结构
+
+	// 实现item的点击
+	public OnItemClickListener listener = new OnItemClickListener() {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			Toast.makeText(Homepage.this,
+					(String) data.get(position).get("news"), Toast.LENGTH_LONG)
+					.show();
+		}
+	};
+
+	// 产生《》结构
 	private List<Hashtable<String, Object>> getData() {
 		List<Hashtable<String, Object>> list = new ArrayList<Hashtable<String, Object>>();
 		for (int i = 0; i < news.length; i++) {
@@ -114,5 +112,63 @@ public class Homepage extends Activity {
 			list.add(table);
 		}
 		return list;
+	}
+
+	// 实现button的点击
+	public final class ViewHolder {
+		public TextView news;
+		public ImageButton button;
+	}
+
+	public class MyAdapter extends BaseAdapter {
+		private LayoutInflater inflater;
+
+		public MyAdapter(Context context) {
+			this.inflater = LayoutInflater.from(context);
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return data.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder = null;
+			if (convertView == null) {
+				holder = new ViewHolder();
+				convertView = inflater.inflate(R.layout.listitem, null);
+				holder.news = (TextView) convertView.findViewById(R.id.news);
+				holder.button = (ImageButton) convertView
+						.findViewById(R.id.like);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+
+			holder.news.setText((String) data.get(position).get("news"));
+			holder.button.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					((ImageButton) v).setImageResource(R.drawable.icon2);
+				}
+			});
+
+			return convertView;
+		}
 	}
 }
